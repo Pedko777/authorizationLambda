8342c1bf07031f687a82a245d80897f53477fa5c
Pedko777 <yuriipedko@gmail.com>
2020-10-26 19:57:54 +0200

    Add instance axios in authOperations

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,22 +1,26 @@
 import axios from 'axios';
 import authActions from './authActions';
 
-axios.defaults.baseURL = 'http://142.93.134.108:1111';
-
-const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  },
+const setAuthToken = token => {
+  if (token) {
+    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete instance.defaults.headers.common['Authorization'];
+  }
 };
+
+const instance = axios.create({
+  baseURL: 'http://142.93.134.108:1111',
+});
 
 const register = credentials => dispatch => {
   dispatch(authActions.registerRequest());
 
-  axios
-    .post('/sign_up', credentials)
+  instance({
+    method: 'POST',
+    url: '/sign_up',
+    data: credentials,
+  })
     .then(response => {
       dispatch(authActions.registerSuccess(response));
     })
@@ -25,11 +29,11 @@ const register = credentials => dispatch => {
 
 const logIn = credentials => dispatch => {
   dispatch(authActions.loginRequest());
-  axios
-    .post(
-      `/login?email=${credentials.email}&password=${credentials.password}`,
-      credentials,
-    )
+  instance({
+    method: 'POST',
+    url: `/login?email=${credentials.email}&password=${credentials.password}`,
+    data: credentials,
+  })
     .then(response => {
       dispatch(authActions.loginSuccess(response));
     })
@@ -49,9 +53,11 @@ const getCurrentUser = () => (dispatch, getState) => {
     const {
       auth: { access_token: tokenAccess },
     } = getState();
-    token.set(tokenAccess);
-    axios
-      .get('/me')
+    setAuthToken(tokenAccess);
+    instance({
+      method: 'GET',
+      url: '/me',
+    })
       .then(response => {
         if (response.data.body.status === 'error') {
           refresh();
@@ -63,29 +69,89 @@ const getCurrentUser = () => (dispatch, getState) => {
   };
 
   const refresh = () => {
+    const {
+      auth: { refresh_token: tokenRefresh },
+    } = getState();
+
     if (!tokenRefresh) {
       return;
     }
     dispatch(authActions.refreshRequest());
-    token.set(tokenRefresh);
-
-    axios
-      .post('/refresh')
+    setAuthToken(tokenRefresh);
+    instance({
+      method: 'POST',
+      url: '/refresh',
+    })
       .then(response => {
-        dispatch(authActions.refreshSuccess(response));
         if (response.data.statusCode === 200) {
+          dispatch(authActions.refreshSuccess(response));
           me();
+        } else {
+          setAuthToken(null);
+          dispatch(authActions.logoutSuccess());
         }
       })
       .catch(error => authActions.refreshError(error));
   };
 
   if (tokenAccess) {
-    token.set(tokenAccess);
+    setAuthToken(tokenAccess);
     me();
     dispatch(authActions.getCurrentUserRequest());
   }
 };
+
+// const getCurrentUser = () => (dispatch, getState) => {
+//   const {
+//     auth: { access_token: tokenAccess, refresh_token: tokenRefresh },
+//   } = getState();
+//   if (!tokenAccess && !tokenRefresh) {
+//     return;
+//   }
+//   const me = () => {
+//     const {
+//       auth: { access_token: tokenAccess },
+//     } = getState();
+//     token.set(tokenAccess);
+//     axios
+//       .get('/me')
+//       .then(response => {
+//         if (response.data.body.status === 'error') {
+//           refresh();
+//         } else {
+//           dispatch(authActions.getCurrentUserSuccess(response));
+//         }
+//       })
+//       .catch(error => authActions.getCurrentUserError(error));
+//   };
+
+//   const refresh = () => {
+//     if (!tokenRefresh) {
+//       return;
+//     }
+//     dispatch(authActions.refreshRequest());
+//     token.set(tokenRefresh);
+
+//     axios
+//       .post('/refresh')
+//       .then(response => {
+//         if (response.data.statusCode === 200) {
+//           dispatch(authActions.refreshSuccess(response));
+//           me();
+//         } else {
+//           token.unset();
+//           dispatch(authActions.logoutSuccess());
+//         }
+//       })
+//       .catch(error => authActions.refreshError(error));
+//   };
+
+//   if (tokenAccess) {
+//     token.set(tokenAccess);
+//     me();
+//     dispatch(authActions.getCurrentUserRequest());
+//   }
+// };
 
 const logOut = () => dispatch => {
   dispatch(authActions.logoutRequest());
